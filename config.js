2a29afdfa2b6bcdca1d2b8d3f23360f437020c87
cmd303 <caominhdang3388@gmail.com>
2016-12-05 11:07:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: '<Your Cloud Messaging API Key from https://console.firebase.google.com>'
+  gcmAPIKey: 'AIzaSyDygqD-Tb2FnZsNblybg32JEXGAYPC-WQw'
 };
